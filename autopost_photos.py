88e36ffc8631ee65b802c30c21dd409c0b5ac1ea
maxchuk89 <maxchuk89@gmail.com>
2025-05-25 14:49:48 +0300

    README сдалал

---
+++ autopost_photos.py
@@ -0,0 +1,37 @@
+import os
+import time
+import random
+from dotenv import load_dotenv
+from telegram import Bot
+
+load_dotenv()
+
+bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+chat_id = os.getenv('TELEGRAM_CHAT_ID')
+post_interval = int(os.getenv('POST_INTERVAL_HOURS', 4)) * 3600
+if not bot_token or not chat_id:
+    raise RuntimeError('Не указан TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID')
+
+bot = Bot(token=bot_token)
+photo_dir = 'space_gallery'
+
+while True:
+    photos = [file for file in os.listdir(photo_dir) if file.lower().endswith(('.jpg', '.png'))]
+
+    if not photos:
+        print('В папке нет фотографий.')
+        break
+
+    random.shuffle(photos)
+
+    for filename in photos:
+        photo_path = os.path.join(photo_dir, filename)
+
+        try:
+            with open(photo_path, 'rb') as photo:
+                bot.send_photo(chat_id=chat_id, photo=photo)
+                print(f'Отправлено: {filename}')
+        except Exception as error:
+            print(f'Ошибка при отправке {filename}: {error}')
+
+        time.sleep(post_interval)
