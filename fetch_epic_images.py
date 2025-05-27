51e29c436ed6a4082b95fb2a0c15de422a270c2b
maxchuk89 <maxchuk89@gmail.com>
2025-05-27 06:45:33 +0300

    Улучшение 5 из 12

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -1,46 +1,36 @@
 import os
 import argparse
+from datetime import datetime
+import requests
 from dotenv import load_dotenv
 
 from image_downloader import download_image
 
 
-from datetime import datetime
-import requests
-
-
-def build_epic_image_links(api_key):
-    if not api_key:
-        raise ValueError('NASA API ключ не задан.')
-
+def get_epic_image_links(api_key, max_images):
     epic_api_endpoint = 'https://api.nasa.gov/EPIC/api/natural'
     request_params = {'api_key': api_key}
-
     response = requests.get(epic_api_endpoint, params=request_params)
     response.raise_for_status()
 
     image_metadata_list = response.json()
     image_links = []
 
-    for image_metadata in image_metadata_list:
+    for image_metadata in image_metadata_list[:max_images]:
         photo_captured_at = datetime.strptime(image_metadata['date'], '%Y-%m-%d %H:%M:%S')
         epic_filename = image_metadata['image']
-
         epic_link = (
             f'https://api.nasa.gov/EPIC/archive/natural/'
             f'{photo_captured_at:%Y/%m/%d}/png/{epic_filename}.png'
             f'?api_key={api_key}'
         )
-
         image_links.append((epic_link, epic_filename))
 
     return image_links
 
 
-def fetch_epic_images(api_key, max_images=10):
-    epic_links_with_filenames = build_epic_image_links(api_key)[:max_images]
-
-    for index, (epic_link, original_filename) in enumerate(epic_links_with_filenames, start=1):
+def download_epic_images(image_links_with_names):
+    for index, (epic_link, _) in enumerate(image_links_with_names, start=1):
         result_filename = f'epic{index}.png'
         final_filepath = os.path.join('space_gallery', result_filename)
         download_image(epic_link, final_filepath)
@@ -59,7 +49,8 @@ def main():
     if not args.api_key:
         raise RuntimeError('NASA_API_KEY не найден в .env и не передан через --api-key')
 
-    fetch_epic_images(args.api_key, max_images=args.count)
+    image_links = get_epic_image_links(args.api_key, args.count)
+    download_epic_images(image_links)
 
 
 if __name__ == '__main__':
