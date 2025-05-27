d41e53589f26cde63f4cd6157057bc4302a25869
maxchuk89 <maxchuk89@gmail.com>
2025-05-27 06:22:12 +0300

    Улучшение 2 из 12

--- publish_photo.py
+++ publish_photo.py
@@ -3,24 +3,30 @@ import random
 from dotenv import load_dotenv
 from telegram import Bot
 
-load_dotenv()
 
-bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-chat_id = os.getenv('TELEGRAM_CHAT_ID')
+def main():
+    load_dotenv()
 
-if not bot_token or not chat_id:
-    raise RuntimeError('Не указан TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID')
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
 
-bot = Bot(token=bot_token)
+    if not bot_token or not chat_id:
+        raise RuntimeError('Не указан TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID')
 
-photo_dir = 'space_gallery'
-photos = [file for file in os.listdir(photo_dir) if file.lower().endswith(('.jpg', '.png'))]
+    bot = Bot(token=bot_token)
 
-if not photos:
-    raise FileNotFoundError('Нет доступных изображений в папке space_gallery')
+    photo_dir = 'space_gallery'
+    photos = [file for file in os.listdir(photo_dir) if file.lower().endswith(('.jpg', '.png'))]
 
-random_photo = random.choice(photos)
-photo_path = os.path.join(photo_dir, random_photo)
+    if not photos:
+        raise FileNotFoundError('Нет доступных изображений в папке space_gallery')
 
-with open(photo_path, 'rb') as photo:
-    bot.send_photo(chat_id=chat_id, photo=photo)
+    random_photo = random.choice(photos)
+    photo_path = os.path.join(photo_dir, random_photo)
+
+    with open(photo_path, 'rb') as photo:
+        bot.send_photo(chat_id=chat_id, photo=photo)
+
+
+if __name__ == '__main__':
+    main()
