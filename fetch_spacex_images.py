0b4557d687a4a532c482629cc5066e053305a7ca
maxchuk89 <maxchuk89@gmail.com>
2025-05-27 06:55:03 +0300

    Удалил воду из названий переменных и функций

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,22 +5,21 @@ import requests
 from image_downloader import download_image
 
 
-def fetch_spacex_images(launch_identifier=None):
-    if not launch_identifier:
-        launch_identifier = '5eb87d47ffd86e000604b38a'
+def fetch_spacex_images(launch_id=None):
+    if not launch_id:
+        launch_id = '5eb87d47ffd86e000604b38a'
 
-    spacex_api_url = f'https://api.spacexdata.com/v5/launches/{launch_identifier}'
-
-    response = requests.get(spacex_api_url)
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+    response = requests.get(url)
     response.raise_for_status()
 
-    launch_metadata = response.json()
-    photo_links = launch_metadata['links']['flickr']['original']
+    launch = response.json()
+    photo_links = launch['links']['flickr']['original']
 
-    for index, photo_link in enumerate(photo_links, start=1):
+    for index, link in enumerate(photo_links, start=1):
         filename = f'spacex{index}.jpg'
-        target_filepath = os.path.join('space_gallery', filename)
-        download_image(photo_link, target_filepath)
+        filepath = os.path.join('space_gallery', filename)
+        download_image(link, filepath)
 
 
 def main():
