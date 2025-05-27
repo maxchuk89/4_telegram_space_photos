d2b01af2735debd7d25241e43378b0be410e140b
maxchuk89 <maxchuk89@gmail.com>
2025-05-27 06:22:12 +0300

    Добавлен if __name__ == '__main__'

--- autopost_photos.py
+++ autopost_photos.py
@@ -4,34 +4,41 @@ import random
 from dotenv import load_dotenv
 from telegram import Bot
 
-load_dotenv()
 
-bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-chat_id = os.getenv('TELEGRAM_CHAT_ID')
-post_interval = int(os.getenv('POST_INTERVAL_HOURS', 4)) * 3600
-if not bot_token or not chat_id:
-    raise RuntimeError('Не указан TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID')
+def main():
+    load_dotenv()
 
-bot = Bot(token=bot_token)
-photo_dir = 'space_gallery'
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    post_interval = float(os.getenv('POST_INTERVAL_HOURS', 4)) * 3600
 
-while True:
-    photos = [file for file in os.listdir(photo_dir) if file.lower().endswith(('.jpg', '.png'))]
+    if not bot_token or not chat_id:
+        raise RuntimeError('Не указан TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID')
 
-    if not photos:
-        print('В папке нет фотографий.')
-        break
+    bot = Bot(token=bot_token)
+    photo_dir = 'space_gallery'
 
-    random.shuffle(photos)
+    while True:
+        photos = [file for file in os.listdir(photo_dir) if file.lower().endswith(('.jpg', '.png'))]
 
-    for filename in photos:
-        photo_path = os.path.join(photo_dir, filename)
+        if not photos:
+            print('В папке нет фотографий.')
+            break
 
-        try:
-            with open(photo_path, 'rb') as photo:
-                bot.send_photo(chat_id=chat_id, photo=photo)
-                print(f'Отправлено: {filename}')
-        except Exception as error:
-            print(f'Ошибка при отправке {filename}: {error}')
+        random.shuffle(photos)
 
-        time.sleep(post_interval)
+        for filename in photos:
+            photo_path = os.path.join(photo_dir, filename)
+
+            try:
+                with open(photo_path, 'rb') as photo:
+                    bot.send_photo(chat_id=chat_id, photo=photo)
+                    print(f'Отправлено: {filename}')
+            except Exception as error:
+                print(f'Ошибка при отправке {filename}: {error}')
+
+            time.sleep(post_interval)
+
+
+if __name__ == '__main__':
+    main()
